0bbca60ea96cec04cf239a493e3972671e92f597
Khondoker Md Mamunur Rashid <mamun1980@gmail.com>
2023-12-08 00:22:30 +0600

    updated requirement file

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,5 +4,8 @@ from .views import BlogHome, my_task
 
 urlpatterns = [
     path('', BlogHome.as_view(), name='blog-home'),
-    path('call/', my_task, name='blog-call')
+    path('call/', my_task, name='blog-call'),
+    path('set-name/', my_task, name='set-name'),
+    path('get-name/', my_task, name='get-name'),
+    path('clear-cache/', my_task, name='clear-cache')
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib import messages
 import random
+from django.core.cache import cache
 from django.conf import settings
 
 from .tasks import mytest_task
@@ -9,6 +10,26 @@ from .tasks import mytest_task
 
 class BlogHome(TemplateView):
     template_name = 'blog/index.html'
+
+
+def set_name(request):
+    cache.set('name', 'Khondoker Md Mamunur Rashid')
+    return render(request, 'blog/index.html')
+
+def set_name(request):
+    name = cache.get('name')
+    context = {
+        "name": name
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def clear_cache(request):
+    cache.clear()
+    context = {
+        "name": None
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def my_task(request):
